69438f5083622b6ddf491792f8f544bd3ce9ef72
SebTota <kidokyo1@gmail.com>
2021-03-26 11:01:45 -0400

    added add tags function to add found tags for a specific photo on both platforms

--- db.py
+++ db.py
@@ -20,16 +20,36 @@ def auth_user(email, password):
         user = auth.sign_in_with_email_and_password(email, password)
         # print(user)
         return True
-    except:
+    except Exception as e:
+        print(f'User failed authentication.')
+        print(e)
         return False
+
+
+def add_tags(platform, user_identifier, photo_identifier, tags):
+    # Ensure correct platform is specified
+    if platform not in ['Android', 'iOS']:
+        raise Exception('Invalid platform specified when trying to add a tag')
+
+    for tag in tags:
+        try:
+            # Set new tag for photo object
+            db.child(f'{platform}/{user_identifier}/Photos/{photo_identifier}/photo_tags/{tag}').set(True)
+            # Set new tag for search tag 'table'
+            db.child(f'{platform}/{user_identifier}/photoTags/{tag}/{photo_identifier}').set(True)
+        except Exception as e:
+            print(f'Failed adding a tag for user: {user_identifier}, photo: {photo_identifier}')
+            print(e)
 
 
 if __name__ == '__main__':
     print('Connecting to firebase')
     # print(auth_user('', ''))
 
-    data = db.child('iOS/sebastiantota/').get()
+    photo_id = 'testPhotoId'
+    tags = ['abc123', '123abc']
 
-    for user in data.each():
-        print(user.key())
-        print(user.val())
+    try:
+        add_tags(platform='iOS', user_identifier='sebastiantota', photo_identifier=photo_id, tags=tags)
+    except Exception as e:
+        print(e)
