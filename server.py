646f9ce8684814600ad0fd4a35d106ca268af2b5
Todge <todgie16@gmail.com>
2021-03-28 19:28:32 -0400

    Created a worker class and added in task queueing for image processing

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify, request, session, redirect
 from uuid import uuid4
+from rq import Queue
+from rq.job import Job
+from worker import conn
 import os
 import object_detection
+import redis
+import json
 
 app = Flask(__name__)
 execution_path = os.path.dirname(os.path.abspath(__file__))
+redis_server = redis.Redis("localhost")
 
+q = Queue(connection=conn)
 
 @app.route('/uploadImage', methods=['POST'])
 def upload_image():
@@ -18,6 +25,49 @@ def upload_image():
 
     upload_file = request.files['file']
     
+    from server import image_upload
+
+    result = q.enqueue(
+        func=image_upload, args=(upload_file, username), result_ttl=5000
+    )
+    print(result)
+    
+    return jsonify({'status': 200, 'labels': redis_server.get(filename)})
+
+@app.route('/getData', methods=['GET'])
+def retrieve_images():
+    print(request.json)
+    content = request.json
+    username = content["username"]
+    password = content["password"]
+    n = 0
+    result = {"numImages": 0, "images": {}}
+    '''
+    result = {
+        "numImages": 0,
+        "images": {
+            "filename": tags,
+            "filename": tags,
+            "filename": tags...
+        }
+    }
+    '''
+    user_string = redis_server.get(username)
+    if user_string is not None:
+        user = json.loads(user_string)
+        result["numImages"] = len(user)
+        result["images"] = user
+    return jsonify({'status': 200, 'results': result})
+
+
+if __name__ == '__main__':
+    print('Importing object detection model')
+    object_detection.import_model()
+
+    print('Starting server')
+    app.run()
+
+def image_upload(upload_file, username):
     # Create a unique file name for this image request
     # Hex encoded to ensure safe filename
     filename = uuid4().hex
@@ -30,36 +80,36 @@ def upload_image():
     # Tags are returned as a list of tuples
     # Ex. [(predicted_tag, probability), ..., (predicted_tag, probability)]
     res = object_detection.process_image(filepath)
+    
+    i = 0
+    taglist = []
+    user = {}
+    '''
+    user = {
+        "filename": tags,
+        "filename": tags,
+        "filename": tags...
+    }
 
-    # Iterate over all the items found
-    for item in res:
-        pred = item[0]  # Prediction
-        prob = item[1]  # Probability
-        print(f'Prediction: {pred}. Probability: {prob}')
+    '''
+    user_string = redis_server.get(username)
+    if user_string is not None:
+        user = json.loads(user_string)
+    else:
+        # Iterate over all the items found and add the tags with a high enough probability to the list
+        for item in res:
+            pred = item[0]  # Prediction
+            prob = item[1]  # Probability
+            if prob > 80:
+                taglist[i] = pred
+                i += 1
+            print(f'Prediction: {pred}. Probability: {prob}')
+        user[filename] = taglist
+        user_string = json.dumps(user)
+        redis_server.set(username, user_string)
+
 
     # Remove file from server once it has been processed
     os.remove(filepath)
 
-    return jsonify({'status': 200, 'labels': 'test, '+username})
-
-
-@app.route('/getData', methods=['GET'])
-def retrieve_images():
-    print(request.json)
-    content = request.json
-    username = content["username"]
-    password = content["password"]
-    n = 0
-    result = {'numImages': n, 'images': {'imageId': [], 'imageId': []}}
-    '''
-    query = query
-    '''
-    return jsonify({'status': 200, 'results': result})
-
-
-if __name__ == '__main__':
-    print('Importing object detection model')
-    object_detection.import_model()
-
-    print('Starting server')
-    app.run()
+    return jsonify({'status': 200, 'labels': user[filename]})
---
+++ worker.py
@@ -0,0 +1,15 @@
+from rq import Worker, Queue, Connection
+import os
+import object_detection
+import redis
+
+listen = ["default"]
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(list(map(Queue, listen)))
+        worker.work()
+
